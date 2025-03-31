59972144bddb1ee0c58342d580d3bd38d6161c0e
Huynh Luu Thien Tan <ttpro115@gmail.com>
2025-03-31 22:34:59 +0700

    xong form dang ki dang nhap

--- shopshoe.sql
+++ shopshoe.sql
@@ -7,6 +7,9 @@
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
+CREATE database shopshoe;
+use shopshoe;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
